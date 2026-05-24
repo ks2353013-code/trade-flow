1c75a328d11f456e88cb523540df47bc6cc7cda9
Krishna Singh <ks2353013@gmail.com>
2026-05-24 08:04:04 -0300

    Fix executive analytics automation model import

--- backend/routes/executiveAnalyticsRoutes.js
+++ backend/routes/executiveAnalyticsRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const AutomationWorkflow = require("../models/AutomationWorkflow");
+const AutomationWorkflow = require("../models/AutomationWorkflow2");
 
 const router = express.Router();
 
